faae9451317589dd977723f2582124c95876ed07
Dominik Straub <dominik.straub@gmail.com>
2022-12-06 20:37:17 +0100

    some optim

--- Sources/Day06/main.swift
+++ Sources/Day06/main.swift
@@ -4,9 +4,9 @@ import Utils
 // let input = try Utils.getInput(bundle: Bundle.module, file: "test")
 let input = try Utils.getInput(bundle: Bundle.module)
 
-func part1() -> Int {
-    input: for (inputIndex, _) in input.enumerated() where inputIndex >= 3 {
-        for searchIndex in inputIndex - 3 ..< inputIndex {
+func loopSearch(length: Int) -> Int? {
+    input: for (inputIndex, _) in input.enumerated() where inputIndex >= (length - 1) {
+        for searchIndex in inputIndex - (length - 1) ..< inputIndex {
             let searchChar = input[searchIndex]
             for matchIndex in searchIndex + 1 ... inputIndex {
                 let matchChar = input[matchIndex]
@@ -17,25 +17,34 @@ func part1() -> Int {
         }
         return inputIndex + 1
     }
-    return -1
+    return nil
+}
+
+func rangeSearch(length: Int) -> Int? {
+    for index in length - 1 ..< input.count where Set(input[index - (length - 1) ... index].map(String.init)).count == length {
+        return index + 1
+    }
+    return nil
+}
+
+func part1() -> Int {
+    return loopSearch(length: 4) ?? -1
+}
+
+func part1Range() -> Int {
+    return rangeSearch(length: 4) ?? -1
 }
 
 _ = run(part: 1, closure: part1)
+_ = run(part: 1, closure: part1Range)
 
 func part2() -> Int {
-    input: for (inputIndex, _) in input.enumerated() where inputIndex >= 13 {
-        for searchIndex in inputIndex - 13 ..< inputIndex {
-            let searchChar = input[searchIndex]
-            for matchIndex in searchIndex + 1 ... inputIndex {
-                let matchChar = input[matchIndex]
-                if searchChar == matchChar {
-                    continue input
-                }
-            }
-        }
-        return inputIndex + 1
-    }
-    return -1
+    return loopSearch(length: 4) ?? -1
+}
+
+func part2Range() -> Int {
+    return rangeSearch(length: 4) ?? -1
 }
 
 _ = run(part: 2, closure: part2)
+_ = run(part: 2, closure: part2Range)
