6602144721201e0448850651a9a4a82643cfdb8e
Dominik Straub <dominik.straub@gmail.com>
2022-12-08 19:15:00 +0100

    Day08, Part2

--- Sources/Day08/main.swift
+++ Sources/Day08/main.swift
@@ -74,8 +74,52 @@ func part1() -> Int {
 
 _ = run(part: 1, closure: part1)
 
-// func part2() -> Int {
-//     return -1
-// }
+func part2() -> Int {
+    var maxScenicScore = -1
+    for currentY in 1 ..< grid.count - 1 {
+        let row = grid[currentY]
+        for currentX in 1 ..< grid[0].count - 1 {
+            let tree = row[currentX]
+            var scenicScore = 1
+            var viewingDistance = 0
+            for y in (0 ..< currentY).reversed() {
+                viewingDistance += 1
+                if grid[y][currentX] >= tree {
+                    break
+                }
+            }
+            scenicScore *= viewingDistance
+            viewingDistance = 0
+            for y in currentY + 1 ..< grid.count {
+                viewingDistance += 1
+                if grid[y][currentX] >= tree {
+                    break
+                }
+            }
+            scenicScore *= viewingDistance
+            viewingDistance = 0
+            for x in (0 ..< currentX).reversed() {
+                viewingDistance += 1
+                if row[x] >= tree {
+                    break
+                }
+            }
+            scenicScore *= viewingDistance
+            viewingDistance = 0
+            for x in currentX + 1 ..< grid[0].count {
+                viewingDistance += 1
+                if row[x] >= tree {
+                    break
+                }
+            }
+            scenicScore *= viewingDistance
 
-// _ = run(part: 2, closure: part2)
+            if scenicScore > maxScenicScore {
+                maxScenicScore = scenicScore
+            }
+        }
+    }
+    return maxScenicScore
+}
+
+_ = run(part: 2, closure: part2)
