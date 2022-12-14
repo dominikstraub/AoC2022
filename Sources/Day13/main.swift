5266d0df508ef08ce14708a9bae100418f540276
Dominik Straub <dominik.straub@gmail.com>
2022-12-14 19:49:12 +0100

    WIP

--- Sources/Day13/main.swift
+++ Sources/Day13/main.swift
@@ -1,24 +1,143 @@
 import Foundation
 import Utils
 
-let input = try Utils.getInput(bundle: Bundle.module, file: "test")
-// let input = try Utils.getInput(bundle: Bundle.module)
+// let input = try Utils.getInput(bundle: Bundle.module, file: "test")
+let input = try Utils.getInput(bundle: Bundle.module)
 
-func prepare() -> [String] {
+func prepare() -> [(List, List)] {
     return input
-        .components(separatedBy: CharacterSet(charactersIn: "\n"))
-        .compactMap { line -> String? in
-            if line == "" {
+        .components(separatedBy: "\n\n")
+        .compactMap { pair -> (List, List)? in
+            if pair == "" {
                 return nil
             }
-            return line
+            let tmp = pair
+                .components(separatedBy: "\n")
+                .compactMap { line -> List? in
+                    if line == "" {
+                        return nil
+                    }
+                    return List(line)
+                }
+            return (tmp[0], tmp[1])
         }
 }
 
-let lines = run(part: "Input parsing", closure: prepare)
+struct List: CustomStringConvertible {
+    let values: [Value]
+    let stringLength: Int
+
+    init(_ int: Int) {
+        stringLength = 1
+        values = [Value.integer(int)]
+    }
+
+    init(_ string: String) {
+        var values = [Value]()
+        var charIndex = 1
+        while charIndex < string.count {
+            if string[charIndex] == "[" {
+                let list = List(String(string[charIndex ..< string.count]))
+                values.append(Value.list(list))
+                charIndex += list.stringLength
+            } else if string[charIndex] == "," {
+                charIndex += 1
+            } else if string[charIndex] == "]" {
+                charIndex += 1
+                break
+            } else {
+                values.append(Value.integer(Int(String(string[charIndex]))!))
+                charIndex += 1
+            }
+        }
+        stringLength = charIndex
+        self.values = values
+    }
+
+    var description: String {
+        return "[\(values.map { $0.description }.joined(separator: ","))]"
+    }
+}
+
+enum Value: CustomStringConvertible {
+    case integer(Int)
+    case list(List)
+
+    var description: String {
+        switch self {
+        case let .integer(int):
+            return "\(int)"
+        case let .list(list):
+            return "\(list)"
+        }
+    }
+}
+
+func compare(_ lhs: Value, _ rhs: Value) -> Int {
+    // print("Compare \(lhs) vs \(rhs)")
+    switch (lhs, rhs) {
+    case let (.integer(lhs), .integer(rhs)):
+        return compare(lhs, rhs)
+    case let (.list(lhs), .list(rhs)):
+        return compare(lhs, rhs)
+    case let (.integer(lhs), .list(rhs)):
+        print("- Compare \(lhs) vs \(rhs)")
+        print("- Mixed types; convert left to \(List(lhs)) and retry comparison")
+        return compare(List(lhs), rhs)
+    case let (.list(lhs), .integer(rhs)):
+        print("- Compare \(lhs) vs \(rhs)")
+        print("- Mixed types; convert right to \(List(rhs)) and retry comparison")
+        return compare(lhs, List(rhs))
+    }
+}
+
+func compare(_ lhs: List, _ rhs: List) -> Int {
+    print("- Compare \(lhs) vs \(rhs)")
+    for listIndex in 0 ..< min(lhs.values.count, rhs.values.count) {
+        let result = compare(lhs.values[listIndex], rhs.values[listIndex])
+        if result != 0 {
+            return result
+        }
+    }
+    if lhs.values.count < rhs.values.count {
+        print("- Left side ran out of items, so inputs are in the right order")
+        return -1
+    } else if lhs.values.count > rhs.values.count {
+        print("- Right side ran out of items, so inputs are not in the right order")
+        return 1
+    } else {
+        return 0
+    }
+}
+
+func compare(_ lhs: Int, _ rhs: Int) -> Int {
+    print("- Compare \(lhs) vs \(rhs)")
+    if lhs < rhs {
+        print("- Left side is smaller, so inputs are in the right order")
+        return -1
+    } else if lhs > rhs {
+        print("- Right side is smaller, so inputs are not in the right order")
+        return 1
+    } else {
+        return 0
+    }
+}
+
+let pairs = run(part: "Input parsing", closure: prepare)
 
 func part1() -> Int {
-    return -1
+    var sum = 0
+    for (pairIndex, pair) in pairs.enumerated() {
+        print("== Pair \(pairIndex + 1) ==")
+        if compare(pair.0, pair.1) != 1 {
+            // print(pairIndex + 1)
+            sum += pairIndex + 1
+        }
+        // print(pair.0)
+        // print(pair.1)
+        print()
+    }
+    return sum
 }
 
 _ = run(part: 1, closure: part1)
